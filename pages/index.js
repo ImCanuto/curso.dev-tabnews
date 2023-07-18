4bc8c68def13fc66c6c27021ffb37bf386744d88
Samuel Canuto <86064467+ImCanuto@users.noreply.github.com>
2023-07-18 20:24:07 +0000

    add a little message to my sweetie

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Hello, Vercel! Hello, world!</h1>
+    return (
+        <div>
+            <h1>Oiii, meu amor! ❤️</h1>
+            <h3>
+                Dei uma pausa nos meus estudos aqui pra dizer que eu te amo mais que tudo nesse mundo
+                e te dizer o quão você é especial e importante na minha vida!
+            </h3>
+            <h5>
+                Ps: Esse link sempre vai estar disponível pra você e pra internet toda saber o tanto que eu te amo!
+            </h5>
+        </div>
+    );
 }
 
 export default Home;
