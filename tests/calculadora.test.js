380b86784fe9e6ac24a3a05a704a2c54e03c5469
Samuel Canuto <86064467+ImCanuto@users.noreply.github.com>
2023-08-24 17:10:19 +0000

    add some experimental tests

---
+++ tests/calculadora.test.js
@@ -0,0 +1,11 @@
+test("nome do teste", () => {
+  console.log("e agora?");
+});
+
+test("testando outra coisa", () => {
+  console.log("outro teste");
+});
+
+test("espero que 1 + 1 seja 2", () => {
+  expect(1 + 1).toBe(2);
+});
