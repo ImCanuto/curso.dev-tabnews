f91d86e68df1c53c20afe578c87f2a7d6b92a622
Samuel Canuto <86064467+ImCanuto@users.noreply.github.com>
2023-09-03 21:10:45 +0000

    add some folder alterations

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import styles from "./styles.module.css"; // Importe o arquivo CSS module
+import styles from "./css/styles.module.css"; // Importe o arquivo CSS module
 
 function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.tittle}>Em desenvolvimento... 🚧</h1>;
+      <h1 className={styles.tittle}>Em desenvolvimento... 🚧</h1>
     </div>
   );
 }
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,11 +1,16 @@
-test("nome do teste", () => {
-  console.log("e agora?");
+const calculadora = require("../models/calculadora.js");
+
+test("somar 2 + 2 deveria retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
 });
 
-test("testando outra coisa", () => {
-  console.log("outro teste");
+test("somar 5 + 100 deveria retornar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
 });
 
-test("espero que 1 + 1 seja 2", () => {
-  expect(1 + 1).toBe(2);
+test("somar 'banana' + 100 deveria retornar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
 });
