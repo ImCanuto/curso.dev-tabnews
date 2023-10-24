a612c515105119d7e2dae70206111415c7ff6766
Samuel Canuto <86064467+ImCanuto@users.noreply.github.com>
2023-10-24 15:45:53 +0000

    add `compose` config file with database service

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,5 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  // async: torna o método assíncrono
+  const response = await fetch("http://localhost:3000/api/v1/status"); // await: faz com que o método aguarde o retorno do fetch
   expect(response.status).toBe(200);
 });
